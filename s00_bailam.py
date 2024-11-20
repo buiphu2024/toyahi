aabee0f8459cef90097ab79e8108a95804f6e1f1
buiphu2024 <pnphu200202@gmail.com>
2024-11-20 06:45:16 +0000

    buiphu nop bai 2

--- s00_bailam.py
+++ s00_bailam.py
@@ -30,6 +30,18 @@ hi('1', '22', '333', '4444') | Hi 1, 22, 333, and 4444!
 #endregion debai
 
 #region bailam
-def hi(*args):
-   return 'TODO'
+def hi(*args, name=None):
+    if name:
+        return f"Hi {name}!"
+    # Remove any empty or None values from args
+    args = [arg for arg in args if arg]
+    # If no valid names are provided, return "Hi!"
+    if not args:
+        return "Hi!"
+    if len(args) == 1:
+        return f"Hi {args[0]}!"
+    # If two names are provided, return greeting with "and"
+    if len(args) == 2:
+        return f"Hi {args[0]} and {args[1]}!"
+    return f"Hi {', '.join(args[:-1])}, and {args[-1]}!"
 #endregion bailam
